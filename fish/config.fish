6aaae2bea31f1d7fd51a666ad6add842d1e9cf70
pedrocarne <pedrohenriquecarneiro143@gmail.com>
2023-01-21 14:56:13 -0300

    updating config files from fish and kitty

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	#ASDF INSTALL
-	source ~/.asdf/asdf.fish
-	
+	source ~/.asdf/asdf.fish 
+
 	#ALIASES
 	alias temp="cd /tmp"
 	alias exa="exa --icons"
 	alias la="exa -lha"
 	alias ll="exa -lh"
-	alias fish-conf="nvim $HOME/.dotfiles/config.fish"
 	alias testf="touch test.txt"
+  alias edit="lvim"
+  alias dnf="dnf -y"
 end
 
 
